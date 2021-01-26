6b56808856cec35649037372685dc5722ba9bca3
gdrivex <77957134+gdrivex@users.noreply.github.com>
2021-01-26 09:36:58 +0530

    Update SmU4ZjZEYS80VTRDNmZoZ3NvOUlQQT09.php

--- SmU4ZjZEYS80VTRDNmZoZ3NvOUlQQT09.php
+++ SmU4ZjZEYS80VTRDNmZoZ3NvOUlQQT09.php
@@ -4,7 +4,6 @@ $a1 = $channel_meta->a1;
 $a2 = $channel_meta->a2;
 $a3 = $channel_meta->a3;
 $a4 = $channel_meta->a4;
-
 $de_a1 = = my_simple_encrypt($a1, 'd');
 $de_a2 = = my_simple_encrypt($a2, 'd');
 $de_a3 = = my_simple_encrypt($a3, 'd');
